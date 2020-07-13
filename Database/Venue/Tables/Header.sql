dbd63af26f89ef9f0b2a163a0a1c0db69ef50fa8
Dan Hulse <dannythulse@gmail.com>
2020-07-13 16:26:24 +0100

    Fixed decimal issue

--- Database/Venue/Tables/Header.sql
+++ Database/Venue/Tables/Header.sql
@@ -3,8 +3,8 @@ CREATE TABLE [venue].[Header]
 	 -- Column Definitions
 	 [Id] INT IDENTITY(1,1) NOT NULL
 	,[Name] VARCHAR(200) NOT NULL
-	,[Latitude] DECIMAL(4,8) NOT NULL
-	,[Longitude] DECIMAL(4,8) NOT NULL
+	,[Latitude] DECIMAL(8,8) NOT NULL
+	,[Longitude] DECIMAL(8,8) NOT NULL
 
 	 -- Primary Key Constraint
 	,CONSTRAINT [pk_venue_header] PRIMARY KEY CLUSTERED ([Id] ASC)
